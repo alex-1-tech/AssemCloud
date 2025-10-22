67ef4b4927f3aeff435a71f9b7afc5a406c694eb
alex <chistovreal@gmail.com>
2025-10-22 09:36:47 +0300

    Update production settings: set DEBUG to False and disable SSL security features

--- assembler/settings/production.py
+++ assembler/settings/production.py
@@ -3,10 +3,10 @@ import os
 
 from .base import *  # noqa: F403
 
-DEBUG = True
+DEBUG = False
 # ALLOWED_HOSTS = ["votum.asuscomm.com", "37.203.240.41", "localhost", "127.0.0.1"]  # noqa: E501, ERA001
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # noqa: F405, PTH118
 
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = False
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
