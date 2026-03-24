bc3418c7ccd2063ef711b44faa54be8e0c0af9e1
alex <chistovreal@gmail.com>
2026-03-24 14:44:30 +0300

    Update models __init__ to export Phasar01 and Phasar02

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,12 +2,14 @@
 
 from core.models.kalmar32 import Kalmar32
 from core.models.license import License
-from core.models.phasar32 import Phasar32
+from core.models.phasar01 import Phasar01
+from core.models.phasar02 import Phasar02
 from core.models.report import Report
 
 __all__ = [
     "Kalmar32",
     "License",
-    "Phasar32",
+    "Phasar01",
+    "Phasar02",
     "Report",
 ]
