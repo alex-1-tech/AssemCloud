f81bb98d5388da848b23dfcbae634fc86eceb8ce
alex <chistovreal@gmail.com>
2026-09-03 14:42:51 +0300

    remove old models from admin menu

--- core/admin/__init__.py
+++ core/admin/__init__.py
@@ -1,14 +1,10 @@
 from core.admin.equipment import EquipmentAdmin, EquipmentTypeAdmin, ModelAdmin, SchemeAdmin
 from core.admin.license import LicenseAdmin
-from core.admin.oldmodels import Kalmar32Admin, Phasar01Admin, Phasar02Admin
 
 __all__ = [
     "EquipmentAdmin",
     "EquipmentTypeAdmin",
-    "Kalmar32Admin",
     "LicenseAdmin",
     "ModelAdmin",
-    "Phasar01Admin",
-    "Phasar02Admin",
     "SchemeAdmin",
 ]
